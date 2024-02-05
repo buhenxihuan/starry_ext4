ad595a3d434a1c0076e59fda50485fd6e8bbcf1c
buhenxihuan <32200913+buhenxihuan@users.noreply.github.com>
2024-02-05 17:34:59 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,7 +5,7 @@ mkfs.ext4  disk.img
 mkdir -p mnt
 sudo mount -o loop disk.img mnt
 # 根据命令行参数生成对应的测例
-sudo cp -r ./testcases/libc-static/* ./mnt/
+sudo cp -r ./testcases/$1/* ./mnt/
 sudo umount mnt
 rm -rf mnt
 sudo chmod 777 disk.img
