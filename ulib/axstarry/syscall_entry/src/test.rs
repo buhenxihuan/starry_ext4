c3e9b823d075c02c56d74ede42272bb7b7149c07
buhenxihuan <1127751018@qq.com>
2024-02-22 14:08:13 +0800

    modified:   ulib/axstarry/syscall_entry/src/test.rs

--- ulib/axstarry/syscall_entry/src/test.rs
+++ ulib/axstarry/syscall_entry/src/test.rs
@@ -313,7 +313,7 @@ pub const OSTRAIN_TESTCASES: &[&str] = &[
 
 #[allow(dead_code)]
 pub const SDCARD_TESTCASES: &[&str] = &[
-    // "busybox sh ./test_all.sh",
+    "busybox sh ./test_all.sh",
     // "./riscv64-linux-musl-native/bin/riscv64-linux-musl-gcc ./hello.c -static",
     // "./a.out",
     // "./time-test",
@@ -362,7 +362,7 @@ pub const SDCARD_TESTCASES: &[&str] = &[
     // "lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX",
     // "busybox echo context switch overhead",
     // "lmbench_all lat_ctx -P 1 -s 32 2 4 8 16 24 32 64 96",
-    "busybox sh libctest_testcode.sh",
+    // "busybox sh libctest_testcode.sh",
     // "busybox sh lua_testcode.sh",
     // "libc-bench",
     // "busybox sh ./netperf_testcode.sh",
